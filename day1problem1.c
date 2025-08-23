7b323a8a266a037c1b03a5d684c42f3622efd511
EliteCoder457 <codercm123@gmail.com>
2025-08-23 19:31:37 +0530

    Create day1problem1.c

---
+++ day1problem1.c
@@ -0,0 +1,13 @@
+// Write a program to input two numbers and display their sum.
+#include <stdio.h>
+
+int main() {
+float x,y, z;
+printf("Enter the first number: ");
+scanf("%f\n",&x);
+printf("Enter the second number: ");
+scanf("%f\n",&y);
+z=x+y;
+printf("The sum of the two numbers is: %0.1f\n", z);
+    return 0;
+}
