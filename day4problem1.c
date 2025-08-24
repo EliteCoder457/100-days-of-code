36e910a8496dc659bed00f1a8037dab470531367
EliteCoder457 <codercm123@gmail.com>
2025-08-24 09:08:57 +0530

    Create day4problem1.c

---
+++ day4problem1.c
@@ -0,0 +1,14 @@
+// Write a program to swap two numbers without using a third variable.
+#include <stdio.h>
+
+int main() {
+float x, y;
+printf("Enter the first number: ");
+scanf("%f",&x);
+printf("Enter the second number: ");
+scanf("%f",&y);
+printf("After swap first number is: %0.1f\n", y);
+printf("After swap second number is : %0.1f\n",x);
+
+    return 0;
+}
