38e8c5cbf51821f848d390995e52a2c764216c56
EliteCoder457 <codercm123@gmail.com>
2025-09-04 09:44:55 +0530

    Update day12problem1.c

--- day12problem1.c
+++ day12problem1.c
@@ -5,21 +5,21 @@ int main() {
 int x, r;
 printf("enter the number of late days: ");
 scanf("%d",&x);
-if (1<x && x<=7){
+if (1<=x && x<=5){
     r=x*2;
-    printf("Fine: %d",r);
+    printf("Fine: ₹%d",r);
 }
-if (7<x && x<=16){
+if (5<x && x<=10){
     r=x*4;
-    printf("Fine: %d",r);
+    printf("Fine: ₹%d",r);
 }
-if (16<x && x<=22){
+if (10<x && x<=30){
     r=x*6;
-    printf("Fine: %d",r);
+    printf("Fine: ₹%d",r);
 }
-if (22<x && x<=31){
-    r=x*8;
-    printf("Fine: %d",r);
+if (x==31){
+   
+    printf("Membership cancelled");
 }
     return 0;
 }
