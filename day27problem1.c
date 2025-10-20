0d7037748cbae88afa3c90b0b295a7ce24f07cce
EliteCoder457 <codercm123@gmail.com>
2025-10-20 16:52:43 +0530

    Create day27problem1.c

---
+++ day27problem1.c
@@ -0,0 +1,31 @@
+/*Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+*/
+
+#include <stdio.h>
+int main (){
+for(int i=1;i<=9;i+=2){
+    for(int j=1;j<=i;j++){
+        printf("*");
+       
+    } 
+     printf("\n");
+}
+for(int i=7;i>=1;i-=2){
+    for(int j=i;j>=1;j--){
+    printf("*");
+}
+printf("\n");
+}
+
+
+    return 0;
+}
