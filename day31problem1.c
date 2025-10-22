81742ba727b55abe33e3b67892f8629d8c23c429
EliteCoder457 <codercm123@gmail.com>
2025-10-22 14:53:17 +0530

    Update day31problem1.c

--- day31problem1.c
+++ day31problem1.c
@@ -1,31 +1,26 @@
+// Search for an element in an array using linear search.
 #include <stdio.h>
 
 int main() {
-    int n, x, found = 0;
-
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
-    int arr[n];
-    printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+int n,key,found=0;
+printf("enter the number of elements: ");
+scanf("%d",&n);
+int arr[n];
+printf("enter %d elements:\n",n);
+for(int i=0;i<n;i++){
+    scanf("%d",&arr[n]);
+}
+printf("enter the element to search: ");
+scanf("%d",&key);
+for(int i=0;i<n;i++){
+    if(arr[i]==key){
+        printf("element found at position: %d\n",i+1);
+        found=1;
+        break;
     }
-
-    printf("Enter the element to search: ");
-    scanf("%d", &x);
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == x) {
-            printf("Element %d found at position %d\n", x, i + 1);
-            found = 1;
-            break;  
-        }
-    }
-
-    if (!found) {
-        printf("Element %d not found in the array.\n", x);
-    }
-
+}
+if(!found){
+    printf("element not found in the array\n");
+}
     return 0;
 }
