d0537ad2d28cdd34b500364aed4530dea302c3c5
EliteCoder457 <codercm123@gmail.com>
2025-11-19 19:21:04 +0530

    Implement matrix transpose functionality

---
+++ day37problem2.c
@@ -0,0 +1,40 @@
+//Find the transpose of a matrix.
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    printf("Enter number of rows (m): ");
+    scanf("%d", &m);
+    printf("Enter number of columns (n): ");
+    scanf("%d", &n);
+
+    int A[m][n], T[n][m];
+
+    printf("Enter the elements of the %d×%d matrix A:\n", m, n);
+
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            T[j][i] = A[i][j];
+        }
+    }
+
+
+    printf("\nTranspose of the matrix (At):\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", T[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
