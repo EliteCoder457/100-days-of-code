b557655dab0b801342256820bd3d194e6a6f4452
EliteCoder457 <codercm123@gmail.com>
2025-11-21 00:00:27 +0530

    Create day44problem1.c

---
+++ day44problem1.c
@@ -0,0 +1,30 @@
+//Count spaces, digits, and special characters in a string.
+#include <stdio.h>
+int main() {
+    char str[200];
+    int i = 0;
+    int spaces = 0, digits = 0, special = 0;
+    printf("Enter a string: ");
+    gets(str);
+    while (str[i] != '\0') {
+        if (str[i] == ' ') {
+            spaces++;
+        }
+        else if (str[i] >= '0' && str[i] <= '9') {
+            digits++;
+        }
+        else if ((str[i] >= 'A' && str[i] <= 'Z') ||
+                 (str[i] >= 'a' && str[i] <= 'z')) {
+            // alphabet → ignore
+        }
+        else {
+            special++;   // punctuation, symbols, etc.
+        }
+        i++;
+    }
+    printf("Spaces = %d\n", spaces);
+    printf("Digits = %d\n", digits);
+    printf("Special Characters = %d\n", special);
+
+    return 0;
+}
